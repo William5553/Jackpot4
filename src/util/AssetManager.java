e6d5069623bc929b0212fb1b572cc3b24a466d8a
William Kang <33787236+William5553@users.noreply.github.com>
2024-01-12 11:28:57 -0500

    error handle asset

--- src/util/AssetManager.java
+++ src/util/AssetManager.java
@@ -5,7 +5,13 @@ import java.awt.*;
 
 public class AssetManager {
     public static ImageIcon getImageIcon(String fileName) {
-        return new ImageIcon("src/assets/" + fileName);
+        try {
+            return new ImageIcon("src/assets/" + fileName);
+        } catch (Exception e) {
+            System.out.println("Error loading image: " + fileName);
+            System.out.println(e);
+        }
+        return null;
     }
 
     public static Image getImage(String fileName) {
